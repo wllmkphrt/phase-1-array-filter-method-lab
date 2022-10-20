a0d2b0f86a01e6feb1901badff87b740f8614242
William Kay Kephart <wllmkphrt@gmail.com>
2022-10-20 13:16:18 -0400

    commit tree 0ccce0fea3ad38cddf6a551c70ecc5e1556f4b77

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(driverNames, string){
+    return driverNames.filter(function(element){
+        return element.toUpperCase().startsWith(string.toUpperCase());
+    });
+}
+function fuzzyMatch(driverNames, string){
+    return driverNames.filter(function(element){
+        return element.startsWith(string);
+    })
+}
+function matchName(drivers, string){
+    return drivers.filter(function(element){
+        return element.name === string;
+    })
+}
